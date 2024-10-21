9d45563812a8ef797e277335134fd44d3ab33d32
Caleb "Trip" Cook <68445331+LightningFrisk@users.noreply.github.com>
2024-10-21 16:29:43 -0500

    before class work

--- week5_silver/week5_build_silver_layer.py
+++ week5_silver/week5_build_silver_layer.py
@@ -29,15 +29,85 @@ logger = spark.sparkContext._jvm.org.apache.log4j
 logger.LogManager.getLogger("org.apache.spark.util.ShutdownHookManager"). setLevel( logger.Level.OFF )
 logger.LogManager.getLogger("org.apache.spark.SparkEnv"). setLevel( logger.Level.ERROR )
 
-bronze_schema = None
+# 1. Define a `bronze_schema` which describes the Parquet files under the bronze reviews directory on S3
+
+# Uses schema inference
+# spark.read.csv("path")
+
+# Uses a custom schema
+# custom_schema = …
+# spark.read.schema(custom_schema).csv("path")
+
+#  Manually define a schema that matches your data
+#     from pyspark.sql.types import StructType, StructField, StringType
+# player_schema = StructType([
+#    StructField("name", StringType, nullable=false)
+#   ,StructField("gender", StringType, nullable=false)
+#   ,StructField("state", StringType, nullable=false)])
+# players = spark.read.schema(player_schema).csv("path")
+
+# Can also nest struct fields together!
+# nested_schema = StructType([
+#   player_schema
+#  ,StructField("score", IntegerType, nullable=false)])
+
+bronze_schema = StructType([
+   StructField("marketplace", StringType, nullable=false)
+  ,StructField("customer_id", StringType, nullable=false) #could be int
+  ,StructField("product_id", StringType, nullable=false) #could be int
+  ,StructField("product_parent", StringType, nullable=false)
+  ,StructField("product_title", StringType, nullable=false)
+  ,StructField("product_category", StringType, nullable=false)
+  ,StructField("star_rating", IntegerType, nullable=false)
+  ,StructField("helpful_votes", IntegerType, nullable=false)
+  ,StructField("total_votes", IntegerType, nullable=false)
+  ,StructField("vine", StringType, nullable=false)
+  ,StructField("verfied_purchase", StringType, nullable=false) #could be boolean
+  ,StructField("review_headline", StringType, nullable=false)
+  ,StructField("review_body", StringType, nullable=false)
+  ,StructField("purchase_date", StringType, nullable=false) #could be date
+  ])
+
+# 2. TODO Define a streaming dataframe using `readStream` on top of the bronze reviews directory on S3
+    # df = spark.readStream.parquet("path")
+
+    # df = spark \ This won't work, but gives framework on how to do this,but this is kafka so maybe too complicated, i think reading from s3 is just a url
+    #     .readStream \
+    #     .format("kafka") \
+    #     .option("kafka.bootstrap.servers", kafka_bootstrap_servers) \
+    #     .option("subscribe", kafka_topic) \
+    #     .option("startingOffsets", "earliest") \
+    #     .option("maxOffsetsPerTrigger", "1000") \
+    #     .option("kafka.security.protocol", "SASL_SSL") \
+    #     .option("kafka.sasl.mechanism", "SCRAM-SHA-512") \
+    #     .option("kafka.sasl.jaas.config", getScramAuthString(username, password)) \
+    #     .load()
+
+# 3. TODO Register a virtual view on top of that dataframe
 
 bronze_reviews = None
 
+# TODO 4. Define a non-streaming dataframe using `read` on top of the bronze customers directory on S3
+# TODO 5. Register a virtual view on top of that dataframe
+
 bronze_customers = None
+
+# TODO 6. Define a `silver_data` dataframe by:
+#    * joining the review and customer data on their common key of `customer_id`
+#    * applying a business validation rule to prevent unverified reviews in the bronze layer from being loaded into the silver layer
 
 silver_data = None
 
-streaming_query = None
+# 7. Write that silver data to S3 under `s3a://hwe-$CLASS/$HANDLE/silver/reviews` using append mode, a checkpoint location of `/tmp/silver-checkpoint`, and a format of `parquet`
+
+# streaming_query = silver_data.writeStream \
+#     .outputMode("append") \
+#     .format("parquet") \
+#     .option("path", "s3a://hwe-fall-2024/ccook/silver/reviews") \
+#     .option("checkpointLocation", "/tmp/silver-checkpoint") \
+#     .start()
+
+print("\n\n--- Write Complete ---\n\n")
 
 streaming_query.start().awaitTermination()
 
