42d443ce24860d73123826bba76f5cce4c1cc7fa
Caleb "Trip" Cook <68445331+LightningFrisk@users.noreply.github.com>
2024-10-23 16:26:01 -0500

    starting on lab

--- week6_gold/week6_build_gold_layer.py
+++ week6_gold/week6_build_gold_layer.py
@@ -31,7 +31,28 @@ logger.LogManager.getLogger("org.apache.spark.util.ShutdownHookManager"). setLev
 logger.LogManager.getLogger("org.apache.spark.SparkEnv"). setLevel( logger.Level.ERROR )
 
 #Define a Schema which describes the Parquet files under the silver reviews directory on S3
-silver_schema = None
+silver_schema = StructType([
+   StructField("marketplace", StringType(), nullable=False)
+  ,StructField("customer_id", StringType(), nullable=False) 
+  ,StructField("product_id", StringType(), nullable=False) 
+  ,StructField("product_parent", StringType(), nullable=False)
+  ,StructField("product_title", StringType(), nullable=False)
+  ,StructField("product_category", StringType(), nullable=False)
+  ,StructField("star_rating", IntegerType(), nullable=False)
+  ,StructField("helpful_votes", IntegerType(), nullable=False)
+  ,StructField("total_votes", IntegerType(), nullable=False)
+  ,StructField("vine", StringType(), nullable=False)
+  ,StructField("verified_purchase", StringType(), nullable=False) 
+  ,StructField("review_headline", StringType(), nullable=False)
+  ,StructField("review_body", StringType(), nullable=False)
+  ,StructField("purchase_date", StringType(), nullable=False) 
+  ,StructField("review_timestamp", TimestampType(), nullable=False) 
+  ,StructField("customer_name", StringType(), nullable=False)
+  ,StructField("gender", StringType(), nullable=False) 
+  ,StructField("date_of_birth", StringType(), nullable=False)
+  ,StructField("city", StringType(), nullable=False)
+  ,StructField("state", StringType(), nullable=False) 
+  ])
 
 #Define a streaming dataframe using readStream on top of the silver reviews directory on S3
 silver_data = None
@@ -43,7 +64,12 @@ watermarked_data = None
 aggregated_data = None
 
 #Write that aggregate data to S3 under s3a://hwe-$CLASS/$HANDLE/gold/fact_review using append mode and a checkpoint location of `/tmp/gold-checkpoint`
-write_gold_query = None
+write_gold_query = aggregated_data.writeStream \
+    .outputMode("append") \
+    .format("parquet") \
+    .option("path", "s3a://hwe-fall-2024/ccook/gold/fact_review") \
+    .option("checkpointLocation", "/tmp/gold-checkpoint") \
+    .start()
 
 write_gold_query.start().awaitTermination()
 
