59e67958527141b3ec38d82897552c06bd392481
Caleb "Trip" Cook <68445331+LightningFrisk@users.noreply.github.com>
2024-11-04 20:18:54 -0600

    commit

---
+++ week7_bi/challenge.py
@@ -0,0 +1,49 @@
+import os
+from pyspark.sql import SparkSession
+from pyspark.sql.functions import count
+from pyspark.sql.types import StructType, StructField, StringType, TimestampType, IntegerType
+
+# Load environment variables.
+from dotenv import load_dotenv
+load_dotenv()
+
+aws_access_key_id = os.environ.get("AWS_ACCESS_KEY_ID")
+aws_secret_access_key = os.environ.get("AWS_SECRET_ACCESS_KEY")
+
+
+# Create a SparkSession
+spark = SparkSession.builder \
+    .appName("Week6Lab") \
+    .config("spark.sql.shuffle.partitions", "3") \
+    .config('spark.hadoop.fs.s3a.aws.credentials.provider', 'org.apache.hadoop.fs.s3a.SimpleAWSCredentialsProvider') \
+    .config("spark.hadoop.fs.s3a.impl", "org.apache.hadoop.fs.s3a.S3AFileSystem") \
+    .config("spark.hadoop.fs.s3a.access.key", aws_access_key_id) \
+    .config("spark.hadoop.fs.s3a.secret.key", aws_secret_access_key) \
+    .config('spark.jars.packages', 'org.apache.spark:spark-sql-kafka-0-10_2.12:3.1.3,org.apache.hadoop:hadoop-aws:3.2.0,com.amazonaws:aws-java-sdk-bundle:1.11.375,io.delta:delta-core_2.12:1.0.1') \
+    .config('spark.sql.extensions','io.delta.sql.DeltaSparkSessionExtension') \
+    .config('spark.sql.catalog.spark_catalog', 'org.apache.spark.sql.delta.catalog.DeltaCatalog') \
+    .master('local[*]') \
+    .getOrCreate()
+
+#For Windows users, quiet errors about not being able to delete temporary directories which make your logs impossible to read...
+logger = spark.sparkContext._jvm.org.apache.log4j
+logger.LogManager.getLogger("org.apache.spark.util.ShutdownHookManager"). setLevel( logger.Level.OFF )
+logger.LogManager.getLogger("org.apache.spark.SparkEnv"). setLevel( logger.Level.ERROR )
+
+#load data
+animal_bites_data = spark.read.csv("resources/animal-bites.csv", sep=",", header=True)
+
+
+what_bit = (
+    animal_bites_data.groupBy("ANIMAL_TYPE","WARD")
+    .count()
+    .orderBy("count", ascending=False)
+)
+what_bit.show()
+
+what_bit.write.parquet("s3a://hwe-fall-2024/ccook/challenge", mode="overwrite")
+
+print("\n -- Writing Data to Output Location -- \n")
+
+## Stop the SparkSession
+spark.stop()
